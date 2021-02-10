f5aab5db174a1ea96a321ade67db11370d55c2db
Miguel Manjarres <tonycortesano@gmail.com>
2021-02-10 11:18:32 -0500

    Se agrega el global navigation guard para verificar cuales rutas requieren de autenticación y bloquearlas al usuario no registrado. Se agregó el history mode a la configuración del router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,41 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
+import { auth } from "@/firebaseconfig";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/login",
     name: "Login",
     component: Login,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/home",
+    redirect: "/",
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const userIsLoggedIn = !!auth.currentUser;
+  if (to.matched.some(record => record.meta.requiresAuth) && !userIsLoggedIn) {
+    next("/login");
+  }
+  next();
 });
 
 export default router;
