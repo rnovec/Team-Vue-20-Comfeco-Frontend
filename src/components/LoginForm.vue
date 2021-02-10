4e2b775de63ad6c4e3c469d73375b29a5757dfee
Miguel Manjarres <tonycortesano@gmail.com>
2021-02-10 17:12:23 -0500

    Refactorización de código para las vista de Login y el componente de LoginForm.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,124 +1,119 @@
 <template>
-  <section class="hero is-light is-fullheight">
-    <div class="hero-body">
-      <div class="container is-fluid">
-        <div class="column is-8 is-offset-2">
-          <div class="box">
-            <div class="column has-text-centered">
-              <div id="nav">
-                <p
-                  class="is-inline is-bold has-text-grey-dark"
-                  style="cursor: pointer"
-                  :class="{ 'active-state': isLogin }"
-                  @click="changeState('login')"
-                >
-                  Iniciar Sesión
-                </p>
-                |
-                <p
-                  class="is-inline is-bold has-text-grey-dark"
-                  style="cursor: pointer"
-                  :class="{ 'active-state': isRegistering }"
-                  @click="changeState('register')"
-                >
-                  Regístrate
-                </p>
-                |
-                <p
-                  class="is-inline is-bold has-text-grey-dark"
-                  style="cursor: pointer"
-                  :class="{ 'active-state': isPasswordRecovery }"
-                  @click="changeState('recovery')"
-                >
-                  Recuperar cuenta
-                </p>
-              </div>
-            </div>
-            <form class="mt-2" @submit.prevent="handleFormSubmit">
-              <div class="columns">
-                <div class="column" v-if="isRegistering">
-                  <b-field label="Nick" icon-right="email">
-                    <b-input
-                      icon="account"
-                      v-model="nickname"
-                      placeholder="my-awesome-nickname"
-                      required
-                    ></b-input>
-                  </b-field>
-                </div>
-                <div class="column">
-                  <b-field label="Correo electrónico">
-                    <b-input
-                      type="email"
-                      icon="email"
-                      v-model="email"
-                      placeholder="somebody@example.com"
-                      required
-                    ></b-input>
-                  </b-field>
-                </div>
-              </div>
-              <div class="columns">
-                <div class="column" v-if="!isPasswordRecovery">
-                  <b-field label="Contraseña">
-                    <b-input
-                      type="password"
-                      v-model="password"
-                      password-reveal
-                      icon="lock"
-                      required
-                    >
-                    </b-input>
-                  </b-field>
-                </div>
-                <div class="column" v-if="isRegistering">
-                  <b-field label="Confirmar contraseña">
-                    <b-input
-                      type="password"
-                      v-model="confirmPassword"
-                      password-reveal
-                      icon="lock-open"
-                      required
-                    >
-                    </b-input>
-                  </b-field>
-                </div>
-              </div>
-              <b-field v-if="!isPasswordRecovery">
-                <b-checkbox size="is-small">
-                  Mantenerme conectado
-                </b-checkbox>
-              </b-field>
-              <b-button
-                type="is-primary"
-                expanded
-                fullwidth
-                native-type="submit"
-                :loading="loading"
-              >
-                {{ isPasswordRecovery ? "Recuperar cuenta" : "Ingresar" }}
-              </b-button>
-              <br />
-              <div class="has-text-centered">
-                <small>
-                  Tambien puedes iniciar con tus redes
-                </small>
-                <br />
-                <br />
-                <b-button size="is-small" type="is-link" icon-left="facebook">
-                  Ingresar con Facebook
-                </b-button>
-                &nbsp;
-                <b-button size="is-small" icon-left="google">
-                  Ingresar con Google
-                </b-button>
-              </div>
-            </form>
-          </div>
-        </div>
+  <div class="box">
+    <div class="column has-text-centered">
+      <div id="nav">
+        <p
+          class="is-inline is-bold has-text-grey-dark"
+          style="cursor: pointer"
+          :class="{ 'active-state': isLogin }"
+          role="button"
+          @click="changeState('login')"
+        >
+          Iniciar Sesión
+        </p>
+        |
+        <p
+          class="is-inline is-bold has-text-grey-dark"
+          style="cursor: pointer"
+          :class="{ 'active-state': isRegistering }"
+          role="button"
+          @click="changeState('register')"
+        >
+          Regístrate
+        </p>
+        |
+        <p
+          class="is-inline is-bold has-text-grey-dark"
+          style="cursor: pointer"
+          :class="{ 'active-state': isPasswordRecovery }"
+          role="button"
+          @click="changeState('recovery')"
+        >
+          Recuperar cuenta
+        </p>
       </div>
     </div>
-  </section>
+    <form class="mt-2" @submit.prevent="handleFormSubmit">
+      <div class="columns">
+        <div class="column" v-if="isRegistering">
+          <b-field label="Nick" icon-right="email">
+            <b-input
+              icon="account"
+              v-model="nickname"
+              placeholder="my-awesome-nickname"
+              required
+            ></b-input>
+          </b-field>
+        </div>
+        <div class="column">
+          <b-field label="Correo electrónico">
+            <b-input
+              type="email"
+              icon="email"
+              v-model="email"
+              placeholder="somebody@example.com"
+              required
+            ></b-input>
+          </b-field>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column" v-if="!isPasswordRecovery">
+          <b-field label="Contraseña">
+            <b-input
+              type="password"
+              v-model="password"
+              password-reveal
+              icon="lock"
+              required
+            >
+            </b-input>
+          </b-field>
+        </div>
+        <div class="column" v-if="isRegistering">
+          <b-field label="Confirmar contraseña">
+            <b-input
+              type="password"
+              v-model="confirmPassword"
+              password-reveal
+              icon="lock-open"
+              required
+            >
+            </b-input>
+          </b-field>
+        </div>
+      </div>
+      <b-field v-if="!isPasswordRecovery">
+        <b-checkbox size="is-small">
+          Mantenerme conectado
+        </b-checkbox>
+      </b-field>
+      <b-button
+        type="is-primary"
+        expanded
+        fullwidth
+        native-type="submit"
+        :loading="loading"
+      >
+        {{ isPasswordRecovery ? "Recuperar cuenta" : "Ingresar" }}
+      </b-button>
+      <br />
+      <div class="has-text-centered">
+        <small>
+          Tambien puedes iniciar con tus redes
+        </small>
+        <br />
+        <br />
+        <b-button size="is-small" type="is-link" icon-left="facebook">
+          Ingresar con Facebook
+        </b-button>
+        &nbsp;
+        <b-button size="is-small" icon-left="google">
+          Ingresar con Google
+        </b-button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -141,65 +136,7 @@
       },
       handleFormSubmit: function() {
         this.loading = true;
-        if (this.isLogin) {
-          if (this.password.length >= 8) {
-            fb.auth
-              .signInWithEmailAndPassword(this.email, this.password)
-              .then(async user => {
-                // El usuario ha iniciado sesión con éxito
-                // Este es el token de autenticacion
-                const idToken = await fb.auth.currentUser.getIdToken();
-                // Este es el refresh token
-                const { refreshToken } = user.user;
-                console.log(idToken);
-                console.log(refreshToken);
-                this.loading = false;
-                this.$router.push("/home");
-              })
-              .catch(error => {
-                this.loading = false;
-                const { message } = error;
-                this.fireErrorAlert(message);
-              });
-          } else {
-            this.loading = false;
-            this.fireErrorAlert("La contraseña proporcionada es muy corta");
-          }
-        } else if (this.isRegistering) {
-          if (this.password.length >= 8) {
-            if (this.password === this.confirmPassword) {
-              fb.auth
-                .createUserWithEmailAndPassword(this.email, this.password)
-                .then(async user => {
-                  // El usuario se ha registrado correctamente
-                  // Este es el token de autenticacion
-                  const idToken = await fb.auth.currentUser.getIdToken();
-                  // Este es el refresh token
-                  const { refreshToken } = user.user;
-                  console.log(idToken);
-                  console.log(refreshToken);
-                  await fb.auth.currentUser.updateProfile({
-                    displayName: this.nickname,
-                  });
-                  this.loading = false;
-                  this.$router.push("/home");
-                })
-                .catch(error => {
-                  this.loading = false;
-                  const { message } = error;
-                  this.fireErrorAlert(message);
-                });
-            } else {
-              this.loading = false;
-              this.fireErrorAlert(
-                "Las contraseñas de confirmación no coinciden. Por favor, revise"
-              );
-            }
-          } else {
-            this.loading = false;
-            this.fireErrorAlert("La contraseña proporcionada es muy corta");
-          }
-        } else if (this.isPasswordRecovery) {
+        if (this.isPasswordRecovery) {
           fb.auth
             .sendPasswordResetEmail(this.email)
             .then(() => {
@@ -212,6 +149,61 @@
                 `El correo no pudo ser enviado, causa del error: ${error.message}`
               );
             });
+        } else {
+          if (this.password.length >= 8) {
+            if (this.isLogin) {
+              fb.auth
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(async user => {
+                  // El usuario ha iniciado sesión con éxito
+                  // Este es el token de autenticacion
+                  const idToken = await fb.auth.currentUser.getIdToken();
+                  // Este es el refresh token
+                  const { refreshToken } = user.user;
+                  console.log(idToken);
+                  console.log(refreshToken);
+                  this.loading = false;
+                  this.$router.push("/home");
+                })
+                .catch(error => {
+                  this.loading = false;
+                  const { message } = error;
+                  this.fireErrorAlert(message);
+                });
+            } else if (this.isRegistering) {
+              if (this.password === this.confirmPassword) {
+                fb.auth
+                  .createUserWithEmailAndPassword(this.email, this.password)
+                  .then(async user => {
+                    // El usuario se ha registrado correctamente
+                    // Este es el token de autenticacion
+                    const idToken = await fb.auth.currentUser.getIdToken();
+                    // Este es el refresh token
+                    const { refreshToken } = user.user;
+                    console.log(idToken);
+                    console.log(refreshToken);
+                    await fb.auth.currentUser.updateProfile({
+                      displayName: this.nickname,
+                    });
+                    this.loading = false;
+                    this.$router.push("/home");
+                  })
+                  .catch(error => {
+                    this.loading = false;
+                    const { message } = error;
+                    this.fireErrorAlert(message);
+                  });
+              } else {
+                this.loading = false;
+                this.fireErrorAlert(
+                  "Las contraseñas de confirmación no coinciden. Por favor, revise"
+                );
+              }
+            }
+          } else {
+            this.loading = false;
+            this.fireErrorAlert("La contraseña proporcionada es muy corta");
+          }
         }
       },
       fireErrorAlert: function(text) {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="home">
-    <LoginForm />
-  </div>
+  <section class="hero is-light is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="column is-8 is-offset-2">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
