3c459a220072a9d949f50ce37dd0b5eba4850157
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-02-10 12:05:36 -0600

    Variables de entorno de firebase

---
+++ firebaseconfig.js
@@ -0,0 +1,18 @@
+import Firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+const app = Firebase.initializeApp(firebaseConfig);
+const db = app.firestore();
+const auth = app.auth();
+
+export { db, auth };
