79b01572fadc1f892dc72d1e085f2d763f6bf84e
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-02-21 19:28:02 -0600

    Close #26. Se agrego carousel de Sponsors

--- src/views/home/components/Sponsors.vue
+++ src/views/home/components/Sponsors.vue
@@ -1,15 +1,16 @@
 <template>
   <carousel
     navigationEnabled
-    class="is-hidden-touch"
     :perPageCustom="[
-      [320, 2],
+      [320, 3],
+      [480, 3],
+      [768, 10],
       [1199, 6],
     ]"
   >
-    <slide v-for="i in items" :key="i.id">
+    <slide v-for="sponsor in sponsors.data" :key="sponsor.id">
       <figure class="image is-64x64 mx-4">
-        <img class="is-rounded" :src="img(i.name_image)" />
+        <img class="is-rounded" :src="img(sponsor.name_image)" />
       </figure>
     </slide>
   </carousel>
@@ -18,15 +19,14 @@
 <script>
   import { Carousel, Slide } from "vue-carousel";
   import sponsors from "@/data-sources/sponsors.json";
+
   export default {
-    components: { Carousel, Slide },
-    computed: {
-      items() {
-        return sponsors.data.map(item => {
-          return item;
-        });
-      },
+    data() {
+      return {
+        sponsors,
+      };
     },
+    components: { Carousel, Slide },
     methods: {
       img(data) {
         return require(`@/assets/sponsors/${data}`);
