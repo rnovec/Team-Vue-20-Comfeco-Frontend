0dfd0c47d7e907ea809d319ec2d768a1c1486708
Kevin Torrez <ktorrez@hansa.com.bo>
2021-02-21 20:49:45 -0400

    Speakers
    Se añadio los lideres de cada tecnologia.

--- src/views/home/components/Speakers.vue
+++ src/views/home/components/Speakers.vue
@@ -1,18 +1,20 @@
 <template>
   <carousel
     autoplay
+    :autoplayTimeout="5000"
+    loop
     class="is-hidden-touch"
     :perPageCustom="[
       [480, 1],
       [768, 4],
     ]"
   >
-    <slide v-for="_ in 10" :key="_">
+    <slide v-for="i in items" :key="i.id">
       <div class="card mx-1">
         <div class="card-image">
           <figure class="image is-1by1">
             <img
-              src="https://www.comfeco.com/images/sponsors/sponsor-fernando_herrera.webp"
+              :src="img(i.name_image)"
               alt="Placeholder image"
             />
           </figure>
@@ -23,10 +25,10 @@
               <figure class="avatar image is-32x32">
                 <img
                   class="is-rounded"
-                  src="https://www.comfeco.com/icons/vue-icon.svg"
+                  :src="technology(i.technology_avatar)"
                 />
               </figure>
-              <p class="title is-7 mt-2">John Smith</p>
+              <p class="title is-7 mt-2">{{i.name}}</p>
             </div>
           </div>
         </div>
@@ -36,21 +38,36 @@
 </template>
 
 <script>
-  import { Carousel, Slide } from "vue-carousel";
-
-  export default {
-    components: { Carousel, Slide },
-  };
+import { Carousel, Slide } from "vue-carousel";
+import speakers from "@/data-sources/speakers.json";
+export default {
+  components: { Carousel, Slide },
+  computed: {
+    items() {
+      return speakers.data.map((item) => {
+        return item;
+      });
+    },
+  },
+  methods: {
+    img(data) {
+      return require(`@/assets/speakers/${data}`);
+    },
+    technology(data) {
+      return require(`@/assets/speakers/technology/${data}`);
+    },
+  },
+};
 </script>
 
 <style>
-  .avatar {
-    position: relative;
-    margin-top: -40px;
-    margin-left: 30%;
-  }
-  .avatar img {
-    background: #fff;
-    border-radius: 50%;
-  }
+.avatar {
+  position: relative;
+  margin-top: -40px;
+  margin-left: 30%;
+}
+.avatar img {
+  background: #fff;
+  border-radius: 50%;
+}
 </style>
