8db13c2fc35b19c4abdf7f272654e5dd53872dc1
Miguel Manjarres <54128874+DevTony101@users.noreply.github.com>
2021-02-28 01:56:37 -0500

    Se edita el componente ProfilePreview.vue para mostrar la información guardada asociada al usuario, incluyendo su biografia y redes sociales

--- src/views/profile/components/ProfilePreview.vue
+++ src/views/profile/components/ProfilePreview.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="card">
     <div class="is-flex is-justify-content-flex-end mt-2 mr-3">
-      <a href=""><small>Editar perfil</small></a>
+      <RouterLink
+        to="/edit-profile"
+        tag="a"
+        class="is-flex is-justify-content-center"
+      >
+        Editar
+        <b-icon pack="fas" icon="cog" />
+      </RouterLink>
     </div>
     <div class="card-image is-flex is-justify-content-center">
-      <figure class="image my-2 is-128x128">
-        <img class="is-rounded" src="https://placehold.it/128x128" />
+      <figure class="image my-2">
+        <img :src="photoUrl" alt="Foto de Perfil" />
       </figure>
     </div>
     <div class="card-content has-text-centered">
@@ -13,19 +20,81 @@
         {{ loguedUser.displayName }}
       </h1>
       <h2 class="subtitle is-7 is-block">
-        Frontend Developer / UX
+        {{ userInfo.area }}
       </h2>
       <div class="content">
         <p>
-          Laborum quis anim velit laboris ea est id mollit nulla qui. Do
-          cupidatat mollit irure aliqua in eu sint culpa non Lorem dolor tempor
-          sint consequat. Ullamco Lorem magna dolor laboris id Lorem adipisicing
-          commodo consequat officia.
+          {{ userInfo.biography }}
         </p>
       </div>
     </div>
-    <div class="card">
-      <footer class="card-footer"></footer>
+    <div class="card-footer p-3 is-justify-content-center">
+      <a :href="facebookReferral" v-if="facebookReferral">
+        <b-icon icon="facebook" />
+      </a>
+      <a :href="twitterReferral" v-if="twitterReferral">
+        <b-icon icon="twitter" />
+      </a>
+      <a :href="githubReferral" v-if="githubReferral">
+        <b-icon pack="fas" icon="code-branch" />
+      </a>
+      <a :href="linkedInReferral" v-if="linkedInReferral">
+        <b-icon icon="linkedin" />
+      </a>
     </div>
   </div>
 </template>
+
+<script>
+  import auth from "@/services/auth";
+  import { usersCollection } from "@/firebaseconfig";
+  export default {
+    name: "ProfilePreview",
+    async mounted() {
+      const result = await usersCollection
+        .where("userId", "==", auth.user.uid)
+        .get();
+      const data = result.docs.length === 0 ? {} : result.docs[0].data();
+      this.userInfo = {
+        area: data.area ?? "Frontend",
+        biography: data.biography ?? "Tu increíble descripción!",
+        ghprofile: data.ghprofile ?? "",
+        twprofile: data.twprofile ?? "",
+        fbprofile: data.fbprofile ?? "",
+        lnprofile: data.lnprofile ?? "",
+      };
+    },
+    data() {
+      return {
+        photoUrl: auth.user.photoURL ?? "https://placehold.it/128x128",
+        userInfo: {},
+      };
+    },
+    computed: {
+      facebookReferral: function() {
+        if (this.userInfo.fbprofile) {
+          return `https://facebook.com/${this.userInfo.fbprofile}`;
+        }
+        return null;
+      },
+      twitterReferral: function() {
+        if (this.userInfo.twprofile) {
+          return `https://twitter.com/${this.userInfo.twprofile}`;
+        }
+        return null;
+      },
+      githubReferral: function() {
+        if (this.userInfo.ghprofile) {
+          return `https://github.com/${this.userInfo.ghprofile}`;
+        }
+        return null;
+      },
+      linkedInReferral: function() {
+        if (this.userInfo.lnprofile) {
+          return `https://linkedin.com/in/${this.userInfo.lnprofile}`;
+        }
+        return null;
+      },
+    },
+  };
+</script>
