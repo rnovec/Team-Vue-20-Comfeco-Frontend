af842a3aa7eabbe2eb46a822756e6a9caf68ec3e
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-03-19 15:06:27 -0600

    Se agrego el bottom nav para movil. Close #86

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,17 +2,20 @@
   <div>
     <Navbar />
     <router-view />
+    <BottomNav />
     <Footer />
   </div>
 </template>
 
 <script>
   import Navbar from "@/components/Navbar";
+  import BottomNav from "@/components/BottomNav";
   import Footer from "@/components/Footer";
   export default {
     name: "DefaultLayout",
     components: {
       Navbar,
+      BottomNav,
       Footer,
     },
   };
